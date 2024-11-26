7d15fcfc95b7d270f3fad34ef51a1aa9599bbe6d
SNS-WE <118381757+SNS-WE@users.noreply.github.com>
2024-11-26 21:52:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -433,10 +433,10 @@ def fill_cems_details(user_id):
         make = st.text_input("Make")
         model = st.text_input("Model")
         serial_number = st.text_input("Serial Number")
-        emission_limit = st.number_input("SPCB Approved Emission Limit", value= None, min_value=0)
+        emission_limit = st.number_input("SPCB Approved Emission Limit", min_value=0)
         measuring_range_low = st.number_input("Measuring Range (Low)", min_value=0)
-        measuring_range_high = st.number_input("Measuring Range (High)", value=None, min_value=0)
-        certified = st.selectbox("Is Certified?", ["Yes", "No"], index=None)
+        measuring_range_high = st.number_input("Measuring Range (High)", min_value=0)
+        certified = st.selectbox("Is Certified?", ["Yes", "No"])
         if certified == "Yes":
             certification_agency = st.text_input("Certification Agency")
         else:
