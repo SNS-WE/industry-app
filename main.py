c2d74e58bc3a2531a6da3f1fd9f1b5ed266c3329
SNS-WE <118381757+SNS-WE@users.noreply.github.com>
2025-01-14 15:44:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1040,9 +1040,9 @@ def fill_cems_details(user_id):
 # Main Function
 def main():
     """Main application logic."""
-    col1, col2, col3, col4, col5 = st.columns([2,1,1,1,1])
+    col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1])
     with col1:
-        st.image("usaid.png" )  # Display logo
+        st.image("USAID.png" )  # Display logo
     with col3:
         st.image("bspcb.png")  # Display logo
     with col5:
