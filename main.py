8bc0ed01b2d67a5c156db6a648a96d2f9790e579
SNS-WE <118381757+SNS-WE@users.noreply.github.com>
2025-01-21 18:49:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1141,6 +1141,7 @@ def main():
     # # Display the custom HTML
     # # st.components.v1.html(custom_html)
     # st.markdown(custom_html, unsafe_allow_html=True)
+    st.image("banner.jpg", caption=None, width=600)
 
     st.markdown(f"<h2 style='text-align: center'>Industry Registration Portal</h1>", unsafe_allow_html=True)
 
