179681fde77f235b9765a54193ae06c380cafaed
SNS-WE <118381757+SNS-WE@users.noreply.github.com>
2025-01-21 17:30:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1113,12 +1113,12 @@ def fill_cems_details(user_id):
 # Main Function
 def main():
     """Main application logic."""
-    col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1,1,1])
+    col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1,1])
     with col1:
         st.image("usaid.png")  # Display logo
-    with col3:
+    with col4:
         st.image("bspcb.png")  # Display logo
-    with col6:
+    with col5:
         st.image("CEEW.png")  # Display logo
 
     st.markdown(f"<h1 style='text-align: center'>Industry Registration Portal</h1>", unsafe_allow_html=True)
