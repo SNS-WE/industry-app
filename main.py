6f6b186c056a59125901ffaea5d303fc637c2f67
SNS-WE <118381757+SNS-WE@users.noreply.github.com>
2025-01-29 12:34:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1116,8 +1116,8 @@ def main():
     col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1,1])
     with col1:
         st.image("bspcb.png")  # Display logo
-    #with col3:
-        #st.image("bspcb.png")  # Display logo
+    with col3:
+        st.title("Industry Registration Portal")  # Display logo
     with col5:
         st.image("CEEW.png")  # Display logo
     
