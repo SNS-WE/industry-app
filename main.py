59b620bcefad2f582c0a6d80ed3cec1c7d7eb27b
SNS-WE <118381757+SNS-WE@users.noreply.github.com>
2025-01-29 12:24:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1115,9 +1115,9 @@ def main():
     """Main application logic."""
     col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1,1])
     with col1:
-        st.image("usaid.png")  # Display logo
-    with col3:
         st.image("bspcb.png")  # Display logo
+    #with col3:
+        #st.image("bspcb.png")  # Display logo
     with col5:
         st.image("CEEW.png")  # Display logo
     
@@ -1142,7 +1142,7 @@ def main():
     """
     st.markdown(reduce_top_margin_style, unsafe_allow_html=True)
 
-    st.image("banner3.jpg", caption=None, use_container_width=True)
+    #st.image("banner3.jpg", caption=None, use_container_width=True)
     
     # st.title("Industry Registration Portal")
     create_database_tables()
